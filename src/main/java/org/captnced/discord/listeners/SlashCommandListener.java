bbb26ae4d840480eeb812cde0395cc6b954ad0ff
captnced <cedric.elsaesser1603@gmail.com>
2025-02-28 17:27:03 +0100

    Added try-catch for command errors

--- src/main/java/org/captnced/discord/listeners/SlashCommandListener.java
+++ src/main/java/org/captnced/discord/listeners/SlashCommandListener.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import org.captnced.discord.messages.Replies;
 import org.captnced.main.Time;
 import org.captnced.untis.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 import java.util.List;
@@ -20,50 +21,65 @@ public class SlashCommandListener extends ListenerAdapter {
     }
 
     @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        if (event.getName().equals("freerooms")) {
-            OptionMapping date = event.getOption("datum");
-            OptionMapping lesson = event.getOption("stunde");
-            if (date == null || lesson == null) {
-                event.replyEmbeds(Replies.error()).setEphemeral(true).queue();
-                return;
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        try {
+            switch (event.getName()) {
+                case "freerooms" -> freerooms(event);
+                case "teachertimetable" -> teachertimetable(event);
             }
-            event.deferReply().queue();
-            untis.requestRooms(date.getAsInt());
-            List<UntisRoom> rooms = untis.filterValidRooms(untis.getFreeRooms(date.getAsInt(), Time.getUntisStartFromLesson(lesson.getAsInt())));
-            String title = "Freie Räume " + Time.formatDate(Time.dateFromUntis(date.getAsInt())) + " - " + lesson.getAsString() + ". Stunde";
-            if (rooms.isEmpty()) {
-                event.getHook().sendMessageEmbeds(Replies.makeEmbed(title, "*Keine freien Räume*", Color.yellow)).queue();
-                return;
+        } catch (Exception e) {
+            if (!event.isAcknowledged()) {
+                event.replyEmbeds(Replies.error(e.getMessage())).queue();
+            } else {
+                event.getHook().sendMessageEmbeds(Replies.error(e.getMessage())).queue();
             }
-            StringBuilder roomNames = new StringBuilder();
-            for (UntisRoom room : rooms) {
-                roomNames.append(room.name()).append("\n");
-            }
-            event.getHook().sendMessageEmbeds(Replies.makeEmbed(title, roomNames.toString(), Color.green)).queue();
-        } else if (event.getName().equals("teachertimetable")) {
-            OptionMapping date = event.getOption("datum");
-            OptionMapping teacher = event.getOption("lehrer");
-            if (date == null || teacher == null) {
-                event.replyEmbeds(Replies.error()).setEphemeral(true).queue();
-                return;
-            }
-            event.deferReply().queue();
-            untis.requestRooms(date.getAsInt());
-            List<UntisPeriod> periods = untis.getTeacherTimetable(untis.findTeacher(teacher.getAsString()), date.getAsInt());
-            String title = "Stunden " + untis.findTeacher(teacher.getAsString()).full() + " - " + Time.formatDate(Time.dateFromUntis(date.getAsInt()));
-            if (periods.isEmpty()) {
-                event.getHook().sendMessageEmbeds(Replies.makeEmbed(title, "*Keine Stunden*", Color.yellow)).queue();
-                return;
-            }
-            periods.sort(Comparator.comparing(UntisPeriod::start));
-            StringBuilder lessons = new StringBuilder();
-            for (UntisPeriod period : periods) {
-                lessons.append(Time.getLessonFromUntisStart(period.start())).append("\\. Stunde: ").append(period.room()).append(" - ").append(period.klass());
-                if (period.canceled()) lessons.append(" (entfällt)");
-                lessons.append("\n");
-            }
-            event.getHook().sendMessageEmbeds(Replies.makeEmbed(title, lessons.toString(), Color.green)).queue();
         }
+    }
+
+    private void freerooms(SlashCommandInteractionEvent event) {
+        OptionMapping date = event.getOption("datum");
+        OptionMapping lesson = event.getOption("stunde");
+        if (date == null || lesson == null) {
+            event.replyEmbeds(Replies.error()).setEphemeral(true).queue();
+            return;
+        }
+        event.deferReply().queue();
+        untis.requestRooms(date.getAsInt());
+        List<UntisRoom> rooms = untis.filterValidRooms(untis.getFreeRooms(date.getAsInt(), Time.getUntisStartFromLesson(lesson.getAsInt())));
+        String title = "Freie Räume " + Time.formatDate(Time.dateFromUntis(date.getAsInt())) + " - " + lesson.getAsString() + ". Stunde";
+        if (rooms.isEmpty()) {
+            event.getHook().sendMessageEmbeds(Replies.makeEmbed(title, "*Keine freien Räume*", Color.yellow)).queue();
+            return;
+        }
+        StringBuilder roomNames = new StringBuilder();
+        for (UntisRoom room : rooms) {
+            roomNames.append(room.name()).append("\n");
+        }
+        event.getHook().sendMessageEmbeds(Replies.makeEmbed(title, roomNames.toString(), Color.green)).queue();
+    }
+
+    private void teachertimetable(SlashCommandInteractionEvent event) {
+        OptionMapping date = event.getOption("datum");
+        OptionMapping teacher = event.getOption("lehrer");
+        if (date == null || teacher == null) {
+            event.replyEmbeds(Replies.error()).setEphemeral(true).queue();
+            return;
+        }
+        event.deferReply().queue();
+        untis.requestRooms(date.getAsInt());
+        List<UntisPeriod> periods = untis.getTeacherTimetable(untis.findTeacher(teacher.getAsString()), date.getAsInt());
+        String title = "Stunden " + untis.findTeacher(teacher.getAsString()).full() + " - " + Time.formatDate(Time.dateFromUntis(date.getAsInt()));
+        if (periods.isEmpty()) {
+            event.getHook().sendMessageEmbeds(Replies.makeEmbed(title, "*Keine Stunden*", Color.yellow)).queue();
+            return;
+        }
+        periods.sort(Comparator.comparing(UntisPeriod::start));
+        StringBuilder lessons = new StringBuilder();
+        for (UntisPeriod period : periods) {
+            lessons.append(Time.getLessonFromUntisStart(period.start())).append("\\. Stunde: ").append(period.room()).append(" - ").append(period.klass());
+            if (period.canceled()) lessons.append(" (entfällt)");
+            lessons.append("\n");
+        }
+        event.getHook().sendMessageEmbeds(Replies.makeEmbed(title, lessons.toString(), Color.green)).queue();
     }
 }
--- src/main/java/org/captnced/discord/messages/Replies.java
+++ src/main/java/org/captnced/discord/messages/Replies.java
@@ -22,4 +22,8 @@ public class Replies {
     public static MessageEmbed error() {
         return makeEmbed("Error", Color.RED);
     }
+
+    public static MessageEmbed error(String message) {
+        return makeEmbed("Error", message, Color.RED);
+    }
 }
